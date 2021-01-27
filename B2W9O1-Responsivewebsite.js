ab113078f0c4985325007be50774a6d5d0b98ff5
99062751 <70890704+99062751@users.noreply.github.com>
2021-01-27 14:32:29 +0100

    update

--- B2W9O1-Responsivewebsite.js
+++ B2W9O1-Responsivewebsite.js
@@ -1,8 +1,12 @@
 var menuitem= document.getElementById("menubar");
 var menutext= document.getElementById("menutext");
+var body= document.getElementsByTagName("body"); 
 menuitem.onclick= menuClicked;
-    menutext.style.display= "none"
 
+
+
+    menutext.style.display= "none";
+   
 
 function menuClicked(){
     if( menutext.style.display == "inline-block"){
