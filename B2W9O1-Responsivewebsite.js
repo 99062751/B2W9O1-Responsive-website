6a48db3edc88dede2a6947255e74dab533eda7ab
99062751 <70890704+99062751@users.noreply.github.com>
2021-01-28 16:19:47 +0100

    update

--- B2W9O1-Responsivewebsite.js
+++ B2W9O1-Responsivewebsite.js
@@ -1,19 +1,26 @@
 var menuitem= document.getElementById("menubar");
 var menutext= document.getElementById("menutext");
-var body= document.getElementsByTagName("body"); 
 menuitem.onclick= menuClicked;
 
-
-
+if(window.innerWidth < 600){
+    menuitem.style.display= "block";
     menutext.style.display= "none";
+
+}else if(window.innerWidth > 600){
+    menuitem.style.display= "none";
+    menutext.style.display= "inline-block";
+}
+
+ 
    
 
 function menuClicked(){
-    if( menutext.style.display == "inline-block"){
+    if(menutext.style.display == "inline-block"){
         menutext.style.display= "none";
     }
-    else{
+    else if(menutext.style.display == "none"){
         menutext.style.display= "inline-block";
     }
     
 }
+
